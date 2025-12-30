5f72d67f9d05ddf42a37180a6f06130545818210
sreeesreejith-jpg <sreee.sreejith@gmail.com>
2025-12-30 09:23:02 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,44 +1,34 @@
-const CACHE_NAME = 'pension-calc-v2';
+const CACHE_NAME = 'pension-calc-v3';
 const ASSETS = [
     './',
     './index.html',
     './style.css',
     './script.js',
     './manifest.json',
-    './icons/icon-192.png',
-    './icons/icon-512.png'
+    './app-icon.png'
 ];
 
-// Install Event
 self.addEventListener('install', (event) => {
     self.skipWaiting();
     event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll(ASSETS);
-        })
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
     );
 });
 
-// Activate Event (Delete old caches)
 self.addEventListener('activate', (event) => {
     event.waitUntil(
         caches.keys().then((cacheNames) => {
             return Promise.all(
                 cacheNames.map((cache) => {
-                    if (cache !== CACHE_NAME) {
-                        return caches.delete(cache);
-                    }
+                    if (cache !== CACHE_NAME) return caches.delete(cache);
                 })
             );
         })
     );
 });
 
-// Fetch Event
 self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
+        caches.match(event.request).then((response) => response || fetch(event.request))
     );
 });
